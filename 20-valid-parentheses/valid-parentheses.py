0a0349505d189ca91b43ec571d97de8de5f5de5b
Ram Abhinav Vedant Madabushi <105885082+ramabhinav2001@users.noreply.github.com>
2026-05-03 08:24:36 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        st=[]
-        mapping={
-            "{":"}",
-            "(":")",
-            "[":"]"
-        }
+        st = []
+    
         for char in s:
-            if char in mapping.values():
-                if not st or mapping[st.pop()]!=char:
+            if char == ')':
+                if not st or st.pop() != '(':
+                    return False
+            elif char == '}':
+                if not st or st.pop() != '{':
+                    return False
+            elif char == ']':
+                if not st or st.pop() != '[':
                     return False
             else:
                 st.append(char)
+        
         return not st
+                
