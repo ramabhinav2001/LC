247a1ac4d73b53c44af7ec107e35117c72c5ce40
Ram Abhinav Vedant Madabushi <105885082+ramabhinav2001@users.noreply.github.com>
2026-05-04 15:07:35 -0400

    Time: 5 ms (52.15%) | Memory: 19.4 MB (19.86%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        romanN={'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
-        res=0
-        for i in range(len(s)):
-            if i+1<len(s) and romanN[s[i]]<romanN[s[i+1]]:
-                res -=romanN[s[i]]
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        numsmap={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        n=len(s)
+        sumn=0
+        for i in range(n):
+            if i+1<n and numsmap[s[i]]<numsmap[s[i+1]]:
+                sumn -=numsmap[s[i]]
             else:
-                res +=romanN[s[i]]
-        return res
-            
-        
+                sumn +=numsmap[s[i]]
+
+        return sumn
