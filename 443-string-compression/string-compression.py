3cdc285376e0fddaeff778e24da570c6e57d29d1
Ram Abhinav Vedant Madabushi <105885082+ramabhinav2001@users.noreply.github.com>
2026-05-06 20:59:15 -0400

    Time: 4 ms (14.30%) | Memory: 19.3 MB (56.64%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,27 +1,19 @@
-class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        write = 0   
-        i = 0    
-
-        while i < len(chars):
-            char = chars[i]
-            count = 0
-
-            while i < len(chars) and chars[i] == char:
-                i += 1
-                count += 1
-
-            chars[write] = char
-            write += 1
-
-            if count > 1:
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        i=0
+        ind=0
+        while i<len(chars):
+            char=chars[i]
+            count=0
+            while i<len(chars) and chars[i]==char:
+                i +=1
+                count +=1
+            chars[ind]=char
+            ind+=1
+            if count>1:
                 for c in str(count):
-                    chars[write] = c
-                    write += 1
-        return write
+                    chars[ind]=c
+                    ind +=1
+        return ind
 
-           
+              
