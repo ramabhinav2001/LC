248ec1f53c381ab8c2917ebe1bf9aabd9e874b51
Ram Abhinav Vedant Madabushi <105885082+ramabhinav2001@users.noreply.github.com>
2026-05-11 11:53:56 -0400

    Time: 6 ms (16.37%) | Memory: 19.4 MB (77.93%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,23 +1,24 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n1=len(nums1)
         n2=len(nums2)
+
         if n1>n2:
             return self.findMedianSortedArrays(nums2,nums1)
-        left = (n1+n2+1) // 2
-        low = 0
-        high = n1
-        n= n1+n2 
-        while low <= high:
-            mid1 = (low+high) // 2
-            mid2 = left-mid1
-            l1,l2,r1,r2=float('-inf'),float('-inf'),float('inf'),float('inf')
+        n=n1+n2
+        left=(n1+n2+1)//2
+        low=0
+        high=n1
+
+        while low<=high:
+            mid1=(low+high)//2
+            mid2=left-mid1
+
+            l1=float('-inf')
+            l2=float('-inf')
+            r1=float('inf')
+            r2=float('inf')
+
             if mid1<n1:
                 r1=nums1[mid1]
             if mid2<n2:
@@ -29,12 +30,10 @@ class Solution(object):
             if l1<=r2 and l2<=r1:
                 if n%2==1:
                     return max(l1,l2)
-                return (max(l1,l2)+min(r1,r2)) / 2.0
-            elif(l1>r2):
+                else:
+                    return (max(l1,l2)+min(r1,r2))/2
+            elif l1>l2:
                 high=mid1-1
             else:
                 low=mid1+1
         return 0
-                
-
-        
